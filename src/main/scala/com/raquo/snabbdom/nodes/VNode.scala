0c33e8c5a292e3f86085d8e075578b6a4300301c
Nikita Gazarov <nikita@raquo.com>
2017-03-13 00:37:17 -0700

    Refactor: Move everything from VNodeOps into RichNode

--- src/main/scala/com/raquo/snabbdom/nodes/VNode.scala
+++ src/main/scala/com/raquo/snabbdom/nodes/VNode.scala
@@ -1,11 +1,13 @@
 package com.raquo.snabbdom.nodes
 
+import com.raquo.interfaces.objectAssign
 import com.raquo.snabbdom.Modifier
 import com.raquo.snabbdom.collections.Builders
 import org.scalajs.dom.raw.Node
 
 import scala.scalajs.js
 import scala.scalajs.js.annotation.{JSName, ScalaJSDefined}
+import scala.scalajs.js.|
 
 class RichNode[N <: VNode](val vnode: N) extends AnyVal {
 
@@ -52,6 +54,58 @@ class RichNode[N <: VNode](val vnode: N) extends AnyVal {
     vnode.children.get.push(child)
   }
 
+  def copy(builders: Builders[N]): N = {
+    // @TODO[NOW] Replace with two-level object-assign
+    val newNode = builders.vnode(tagName = vnode.sel)
+    newNode.sel = vnode.sel
+    newNode.data = copyData(vnode.data)
+    vnode.elm.foreach(elm => newNode.elm = elm)
+    vnode.key.foreach(key => newNode.key = key)
+    vnode.text.foreach(text => newNode.text = text)
+    vnode.children.foreach(children => newNode.children = copyChildren(children))
+    newNode
+  }
+
+  def copyData(data: VNodeData): VNodeData = {
+    val newData = new VNodeData {}
+    data.attrs.foreach { attrs =>
+      newData.attrs = objectAssign(js.Dictionary[Boolean | String](), attrs)
+    }
+    data.on.foreach { on =>
+      newData.on = objectAssign(js.Dictionary[js.Function](), on)
+    }
+    data.props.foreach { props =>
+      newData.props = objectAssign(js.Dictionary[Any](), props)
+    }
+    data.styles.foreach { styles =>
+      newData.styles = objectAssign(js.Dictionary[Any](), styles)
+    }
+    data.hooks.foreach {
+      hooks => newData.hooks = copyHooks(hooks)
+    }
+    newData
+  }
+
+  def copyHooks(hooks: Hooks): Hooks = {
+    new Hooks {
+      init = hooks.init
+      create = hooks.create
+      insert = hooks.insert
+      prePatch = hooks.prePatch
+      update = hooks.update
+      postPatch = hooks.postPatch
+      destroy = hooks.destroy
+      remove = hooks.remove
+    }
+  }
+
+//  def copyChildren(children: js.Array[N]): js.Array[N] = {
+//    children.jsSlice()
+//  }
+
+  def copyChildren(children: js.Array[VNode]): js.Array[VNode] = {
+    children.jsSlice()
+  }
 }
 
 @ScalaJSDefined
