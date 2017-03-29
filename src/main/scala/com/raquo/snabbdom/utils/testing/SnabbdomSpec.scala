fe842b5052e86b17db394b7f01ad00aa37140323
Nikita Gazarov <nikita@raquo.com>
2017-03-28 21:42:33 -0700

    Style: provid eexplicit type for public var snabbdomModules

--- src/main/scala/com/raquo/snabbdom/utils/testing/SnabbdomSpec.scala
+++ src/main/scala/com/raquo/snabbdom/utils/testing/SnabbdomSpec.scala
@@ -1,9 +1,13 @@
 package com.raquo.snabbdom.utils.testing
 
 import com.raquo.snabbdom
-import com.raquo.snabbdom.{VNode, VNodeBuilders, VNodeData}
+import com.raquo.snabbdom.hooks.ModuleHooks
+import com.raquo.snabbdom.{NativeModule, VNode, VNodeBuilders, VNodeData}
 import com.raquo.snabbdom.utils.testing.matching.RuleImplicits
 import org.scalatest.Suite
+
+import scala.scalajs.js
+import scala.scalajs.js.|
 
 trait SnabbdomSpec
   extends MountSpec[VNode, VNodeData]
@@ -12,5 +16,5 @@ trait SnabbdomSpec
   with DomEventSimulatorSpec
 { this: Suite =>
 
-  val snabbdomModules = snabbdom.modules
+  override val snabbdomModules: js.Array[NativeModule | ModuleHooks[VNode, VNodeData]] = snabbdom.modules
 }
